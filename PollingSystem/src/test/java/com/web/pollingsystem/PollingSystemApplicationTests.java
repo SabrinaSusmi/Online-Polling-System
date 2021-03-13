0b9dc06999a9e177396df140cee955bb24ddb4b4
SabrinaSusmi <sabrina.su512@gmail.com>
2021-03-13 20:34:27 +0600

    project setup

---
+++ PollingSystem/src/test/java/com/web/pollingsystem/PollingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.pollingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
